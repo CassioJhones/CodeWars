0a5aea7518e03a7c0f3e3d208118a88c1230397e
CASSIO <cassio.bjhones@gmail.com>
2024-01-14 01:50:22 -0300

    Jaden Casing Strings

---
+++ JadenCasingStrings.cs
@@ -0,0 +1,14 @@
+﻿string jaden = "How can mirrors be real if our eyes aren't real";
+string[] fraseCortada = jaden.Split(' ');
+string fraseCompleta = "";
+
+foreach (string palavra in fraseCortada)
+{
+    char capturaLetraInicial = palavra[0];
+    char letraMaiuscula = char.ToUpper(capturaLetraInicial);
+    string Frase = letraMaiuscula + palavra.Substring(1);
+    fraseCompleta += $"{Frase} ";
+}
+
+Console.WriteLine($"Not Jaden-Cased: \"{jaden}\"");
+Console.WriteLine($"Jaden-Cased:     \"{fraseCompleta.TrimEnd()}\"");
